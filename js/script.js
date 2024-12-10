b29f823071a7c54f4c4c1454d6403d5799174178
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-10 17:37:13 +0100

    feat: add ticket's placeholder and ticket variables

--- js/script.js
+++ js/script.js
@@ -1,13 +1,32 @@
 // DOM ELEMENTS
+
 // Fields
+
 const fullNameFieldElm = document.getElementById("full-name-field")
 const distanceFieldElm = document.getElementById("distance-field")
 const ageSelectorElm = document.getElementById("age-selector")
+
+// Placeholder ticket
+
+const namePassenger = document.getElementById("namePassenger")
+const discountForAge = document.getElementById("discount")
+const randomSeat = document.getElementById("seat")
+const randomCpCode = document.getElementById("cp-code")
+const ticketPrice = document.getElementById("ticket-price")
+
 // Forms
+
 const formElm = document.getElementById("form")
 
 // console.log(fullNameFieldElm, distanceFieldElm, ageSelectorElm, formElm)
 
+// LOGIC
+
 formElm.addEventListener("submit", function(event) {
     event.preventDefault()
+
+    // Inserimento dati
+    namePassenger.innerHTML = fullNameFieldElm.value
+    discountForAge.innerHTML = ageSelectorElm.value
 })
+
