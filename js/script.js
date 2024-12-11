8b76731db194b2cf03889306397b1ac8caacb7b3
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-11 10:27:37 +0100

    fix: add € to tickets price

--- js/script.js
+++ js/script.js
@@ -54,14 +54,14 @@ formElm.addEventListener("submit", function(event) {
     randomSeatElm.innerHTML = getRandomNumber(1, 10)
     randomCpCodeElm.innerHTML = getRandomNumber(90000, 100000)
     if (ageSelectorElm.value === "Maggiorenne") {
-        ticketPriceElm.innerHTML = ticketPrice
-        console.log(fullNameFieldElm.value, ageSelectorElm.value, randomSeatElm.innerHTML, randomCpCodeElm.innerHTML, ticketPrice)
+        ticketPriceElm.innerHTML = ticketPrice + "€"
+        console.log(fullNameFieldElm.value, ageSelectorElm.value, randomSeatElm.innerHTML, randomCpCodeElm.innerHTML, ticketPrice + "€")
     } else if (ageSelectorElm.value === "Minorenne") {
-        ticketPriceElm.innerHTML = ticketPriceUnderage
-        console.log(fullNameFieldElm.value, ageSelectorElm.value, randomSeatElm.innerHTML, randomCpCodeElm.innerHTML, ticketPriceUnderage)
+        ticketPriceElm.innerHTML = ticketPriceUnderage + "€"
+        console.log(fullNameFieldElm.value, ageSelectorElm.value, randomSeatElm.innerHTML, randomCpCodeElm.innerHTML, ticketPriceUnderage + "€")
     } else if (ageSelectorElm.value === "Over 65") {
-        ticketPriceElm.innerHTML = ticketPriceOver65
-        console.log(fullNameFieldElm.value, ageSelectorElm.value, randomSeatElm.innerHTML, randomCpCodeElm.innerHTML, ticketPriceOver65)
+        ticketPriceElm.innerHTML = ticketPriceOver65 + "€"
+        console.log(fullNameFieldElm.value, ageSelectorElm.value, randomSeatElm.innerHTML, randomCpCodeElm.innerHTML, ticketPriceOver65 + "€")
     }
     sectionTicketElm.classList.remove("d-none")
     sectionTicketElm.classList.add("d-block")
